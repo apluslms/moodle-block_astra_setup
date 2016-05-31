166797c514e03b906e10a21ebafc7130c23332e5
Markku Riekkinen <markku.riekkinen@aalto.fi>
2016-05-31 13:58:27 +0300

    Update version.php for the first release.

--- stratumtwo_setup/version.php
+++ stratumtwo_setup/version.php
@@ -2,10 +2,10 @@
 
 $plugin->component = 'block_stratumtwo_setup'; // Declare the type and name of this plugin.
 // YYYYMMDDHH (year, month, day, 24-hr time/version counter of the day)
-$plugin->version = 2016031600;
+$plugin->version = 2016053100;
 $plugin->requires = 2015111600; // Moodle 3.0
 $plugin->release = 'v0.1';
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'mod_stratumtwo' => 2016021900,
+    'mod_stratumtwo' => 2016053100,
 );
