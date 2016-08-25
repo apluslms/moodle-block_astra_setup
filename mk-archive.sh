d37ba784c7eaa64449870bc77e2ca96dbd97609b
Markku Riekkinen <markku.riekkinen@aalto.fi>
2016-08-25 11:19:28 +0300

    Rename plugin directory (plugin name change).

--- mk-archive.sh
+++ mk-archive.sh
@@ -2,4 +2,4 @@
 
 # make a tar archive of the directory that is installed in Moodle
 
-git archive -o block_stratumtwo_setup.tgz master stratumtwo_setup/
+git archive -o block_astra_setup.tgz master astra_setup/
