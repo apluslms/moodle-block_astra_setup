4a0a9ac89d617cbb91f089f38bfc6605bb73833e
Markku Riekkinen <markku.riekkinen@aalto.fi>
2016-09-06 18:13:39 +0300

    Update version for the first release.

--- astra_setup/version.php
+++ astra_setup/version.php
@@ -2,10 +2,10 @@
 
 $plugin->component = 'block_astra_setup'; // Declare the type and name of this plugin.
 // YYYYMMDDHH (year, month, day, 24-hr time/version counter of the day)
-$plugin->version = 2016082500;
+$plugin->version = 2016090600;
 $plugin->requires = 2015111600; // Moodle 3.0
-$plugin->release = 'v0.2';
+$plugin->release = 'v1.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'mod_astra' => 2016082400,
+    'mod_astra' => 2016090600,
 );
