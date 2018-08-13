1111f9082da4d80279baace910ff168e6f5990a2
Markku Riekkinen <markku.riekkinen@aalto.fi>
2018-08-13 10:26:29 +0300

    Add Finnish language translations.

---
+++ astra_setup/lang/fi/block_astra_setup.php
@@ -0,0 +1,12 @@
+<?php
+
+$string['pluginname'] = 'Astra-tehtävien asennus'; // required by Moodle core
+
+$string['editexercises'] = 'Muokkaa tehtäviä';
+$string['deviations'] = 'Poikkeavuudet';
+$string['exportdata'] = 'Lataa kurssidataa';
+$string['massregrading'] = 'Massauudelleenarviointi';
+$string['participants'] = 'Osallistujat';
+
+// capability description
+$string['astra_setup:addinstance'] = 'Lisää uusi Astra-tehtävien asennuslohko.';
--- astra_setup/version.php
+++ astra_setup/version.php
@@ -2,9 +2,9 @@
 
 $plugin->component = 'block_astra_setup'; // Declare the type and name of this plugin.
 // YYYYMMDDHH (year, month, day, 24-hr time/version counter of the day)
-$plugin->version = 2017101700;
+$plugin->version = 2018081300;
 $plugin->requires = 2015111600; // Moodle 3.0
-$plugin->release = 'v1.1';
+$plugin->release = 'v1.2';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'mod_astra' => 2017102000,
